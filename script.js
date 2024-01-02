218c7235089b58eea4b5d8be479a95f269613afb
adrianmihail <adrianmihail00@gmail.com>
2024-01-02 19:10:04 +0200

    Trying to push number into array

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let a = 1;
-let b = 2;
+let arr = [];
+console.log(arr);
 
-console.log(a+b);
+const nrSeven = document.querySelector('#seven');
+nrSeven.addEventListener('click', () => {
+    return arr.push(7)
+});
+
+console.log(arr);
+
+// to do = push number into array and store it for future operations

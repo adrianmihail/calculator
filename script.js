83c8f92f5406eb979a894b0f2fefbc56d756ab54
adrianmihail <adrianmihail00@gmail.com>
2024-01-03 17:53:32 +0200

    Add numbers to array on button click

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let inputNumbers = [];
-console.log(inputNumbers);
 
 const nrSeven = document.querySelector('#seven');
 nrSeven.addEventListener('click', () => {
@@ -8,5 +7,95 @@ nrSeven.addEventListener('click', () => {
     console.log(inputNumbers);
 });
 
+const nrEight = document.querySelector('#eight');
+nrEight.addEventListener('click', () => {
+    const value = 8;
+    inputNumbers.push(value);
+    console.log(inputNumbers);
+});
 
-// to do = push number into array and store it for future operations
+const nrNine = document.querySelector('#nine');
+nrNine.addEventListener('click', () => {
+    const value = 9;
+    inputNumbers.push(value);
+    console.log(inputNumbers);
+});
+
+const nrFour = document.querySelector('#four');
+nrFour.addEventListener('click', () => {
+    const value = 4;
+    inputNumbers.push(value);
+    console.log(inputNumbers);
+});
+
+const nrFive = document.querySelector('#five');
+nrFive.addEventListener('click', () => {
+    const value = 5;
+    inputNumbers.push(value);
+    console.log(inputNumbers);
+});
+
+const nrSix = document.querySelector('#six');
+nrSix.addEventListener('click', () => {
+    const value = 6;
+    inputNumbers.push(value);
+    console.log(inputNumbers);
+});
+
+const nrOne = document.querySelector('#one');
+nrOne.addEventListener('click', () => {
+    const value = 1;
+    inputNumbers.push(value);
+    console.log(inputNumbers);
+});
+
+const nrTwo = document.querySelector('#two');
+nrTwo.addEventListener('click', () => {
+    const value = 2;
+    inputNumbers.push(value);
+    console.log(inputNumbers);
+});
+
+const nrThree = document.querySelector('#three');
+nrThree.addEventListener('click', () => {
+    const value = 3;
+    inputNumbers.push(value);
+    console.log(inputNumbers);
+});
+
+const nrZero = document.querySelector('#zero');
+nrZero.addEventListener('click', () => {
+    const value = 0;
+    inputNumbers.push(value);
+    console.log(inputNumbers);
+});
+
+console.log(inputNumbers);
+
+
+/* Other Buttons
+
+*** MAKE DENOMINATOR + - * % with splice in order to separate the numbers *****
+
+const dot = document.querySelector('#seven');
+nrSeven.addEventListener('click', () => {
+    const value = 7;
+    inputNumbers.push(value);
+    console.log(inputNumbers);
+});
+
+const clear = document.querySelector('#seven');
+nrSeven.addEventListener('click', () => {
+    const value = 7;
+    inputNumbers.push(value);
+    console.log(inputNumbers);
+});
+
+const result = document.querySelector('#seven');
+nrSeven.addEventListener('click', () => {
+    const value = 7;
+    inputNumbers.push(value);
+    console.log(inputNumbers);
+});
+
+*/
